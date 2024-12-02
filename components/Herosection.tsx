605d8fed4c2cdf82643ab58be5a76b6319f32f69
waniazaheer <waniawania430@gmail.com>
2024-12-01 15:18:47 -1200

    5 sections added

--- components/Herosection.tsx
+++ components/Herosection.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 const Herosection = () => {
 
   return (
-    <div className="flex items-start">
-    <img className="w-4/6 max-h-[400px] object-contain ml-0" src="https://demo.readyecommerce.app/storage/banners/WXu98g57ALn5cs6ZVhHtQeLmb9G1gMUFxBCWDSiD.jpg" alt="..." />
-   </div>
-  
+
+    <div className="flex justify-start ml-20 gap-9">
+      <img className="w-4/6 max-h-[400px] object-contain " src="https://demo.readyecommerce.app/storage/banners/WXu98g57ALn5cs6ZVhHtQeLmb9G1gMUFxBCWDSiD.jpg" alt="..." />
+      <div className="flex flex-col justify-between " >
+        <img src="https://demo.readyecommerce.app/storage/flash_sales/9s4sbShhVJsnfN7lvolzygY1ehf3uMKA2YeKD3pR.png" alt="" width={250} />
+        <div>
+          <img src="	https://demo.readyecommerce.app/storage/ads/tuFf1DDMt2n2S72tNyDaAHZtg1J57B55Ixv25xGv.jpg" alt="" width={250} />
+        </div>
+      </div>
+    </div>
   )
 }
 
