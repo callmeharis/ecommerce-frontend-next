b8fbf6eacd4f8e8540bde7124cd9f6b39df1f8cb
noor157 <ni9964258@gmail.com>
2024-12-10 13:34:29 +0500

    changing in different files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-import Authmodal from "@/components/Authmodal";
 import Cards from "@/components/Cards";
 import Categoryslider from "@/components/Categoryslider";
 // import FlashSale from "@/components/FlashSale";
@@ -12,7 +10,7 @@ export default function Home() {
       <Herosection/>
        <Cards/>
        <Categoryslider/>
-       <Authmodal/>
+       
        {/* <div className="h-80 bg-pink-200 flex items-center justify-center mb-20">
       <FlashSale />
     </div> */}
